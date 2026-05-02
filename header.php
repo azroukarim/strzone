ac1633a6af87f3bb4d05c7f013ed4668b1195eb9
MARKETTV1 <aliridakarim8@gmail.com>
2026-05-02 15:07:21 +0100

    Fixed mobile menu toggler functionality

--- header.php
+++ header.php
@@ -90,3 +90,14 @@ include_once 'get_status.php';
             </div>
         </div>
     </nav>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const toggler = document.querySelector('.navbar-toggler');
+            const menu = document.querySelector('#streamNavbar');
+            if (toggler && menu) {
+                toggler.addEventListener('click', function() {
+                    menu.classList.toggle('show');
+                });
+            }
+        });
+    </script>
